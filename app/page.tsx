0fefe2d22bf4e4905798695acf9ef4b2713f12dd
Md Sahi Alam Khan <ksahi4088@gmail.com>
2024-11-13 21:10:55 +0530

    code commented map

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
             <Visibility />
             <Pressure />
           </div>
-          <div className="mapbox-con mt-4 flex gap-4">
+          {/* <div className="mapbox-con mt-4 flex gap-4">
             <Mapbox />
             <div className="states flex flex-col gap-3 flex-1">
               <h2 className="flex items-center gap-2 font-medium">
@@ -82,7 +82,7 @@ export default function Home() {
                 })}
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </main>
