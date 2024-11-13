56a066f679a15cd8d12ef460d78caf2a89b9a00b
Md Sahi Alam Khan <ksahi4088@gmail.com>
2024-11-13 12:42:35 +0530

    testing solution

--- app/page.tsx
+++ app/page.tsx
@@ -33,12 +33,12 @@ export default function Home() {
       <Navbar />
       <div className="pb-4 flex flex-col gap-4 md:flex-row">
         <div className="flex flex-col gap-4 w-full min-w-[18rem] md:w-[35rem]">
-          <Temperature />
-          <FiveDayForecast />
+          {/* <Temperature /> */}
+          {/* <FiveDayForecast /> */}
         </div>
         <div className="flex flex-col w-full">
           <div className="instruments grid h-full gap-4 col-span-full sm-2:col-span-2 lg:grid-cols-3 xl:grid-cols-4">
-            <AirPollution />
+            {/* <AirPollution />
             <Sunset />
             <Wind />
             <DailyForecast />
@@ -47,11 +47,11 @@ export default function Home() {
             <FeelsLike />
             <Humidity />
             <Visibility />
-            <Pressure />
+            <Pressure /> */}
           </div>
           <div className="mapbox-con mt-4 flex gap-4">
-            <Mapbox />
-            <div className="states flex flex-col gap-3 flex-1">
+            {/* <Mapbox /> */}
+            {/* <div className="states flex flex-col gap-3 flex-1">
               <h2 className="flex items-center gap-2 font-medium">
                 Top Large Cities
               </h2>
@@ -70,7 +70,7 @@ export default function Home() {
                   );
                 })}
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
